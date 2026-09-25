388ca9fc22580c976414ce685aaacec8b9f75f89
devtria <devtria249@gmail.com>
2026-09-25 17:50:39 +0700

    Add Dart program to demonstrate data type determination and variable modification

---
+++ Fase 1 — Pengenalan Dart/2_menentukan_tipe_data.dart
@@ -0,0 +1,18 @@
+void main() {
+  String nama = 'Satria Nurulloh';
+  int umur = 21;
+  double iq = 200.0;
+  bool seorangProgramer = true;
+
+  // versi modifikasi
+  String name = 'Satria Nurulloh';
+  umur = 99;
+  iq = 290.0;
+  seorangProgramer = false;
+
+  print(nama);
+  print(name);
+  print(umur);
+  print(iq);
+  print(seorangProgramer);
+}
